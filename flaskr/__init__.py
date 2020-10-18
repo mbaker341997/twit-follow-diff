0d5efb3c23f5c8884b0a3f0821b40270214f5656
mbakerperson <mbaker341997@gmail.com>
2020-10-17 19:26:51 -0700

    Linkified twitter @s in the descrption

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,6 @@
-from flask import Flask
 import bleach
-
+import re
+from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -17,5 +17,15 @@ def create_app(test_config=None):
     @app.template_filter('linkify')
     def linkify_filter(s):
         return bleach.linkify(s)
+
+    def sub_username(username_matchobj):
+        username_string = username_matchobj.group(0)
+        return '<a href="https://twitter.com/{}">{}</a>'.format(username_string[1:], username_string)
+
+    # filter for converting twitter username @s to links
+    @app.template_filter('usernamify')
+    def username_filter(s):
+        # regex from https://stackoverflow.com/questions/2304632/regex-for-twitter-username
+        return re.sub(r'(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9-_]+)', sub_username, s)
 
     return app
